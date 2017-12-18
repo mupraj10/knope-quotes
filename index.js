99597de5c0458874438217b511e07b1864e63416
Maggy Prajapati <mupraj10@gmail.com>
2017-12-18 00:32:12 -0500

    minor edits

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 1337
 
 
 const sendQuote = require('./sendQuote');
