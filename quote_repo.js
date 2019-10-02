e6eec3d089616c70835a416502a07e929ef19989
Dave Van Fleet <dvfleet413@gmail.com>
2019-10-01 23:08:34 -0400

    Update quote_repo.js

--- quote_repo.js
+++ quote_repo.js
@@ -47,6 +47,8 @@ const quotes = [
   "No matter what I do, literally nothing bad can happen to me. I'm like a white male US senator.",
   'I would like to be president some day, so, no, I have not smoked marijuana.',
   'Every great work of art contains a message. And the message of this painting is get out of my way unless you want an arrow in your ass, Marcia.'
+  'Oh Ann, you beautiful, naive, sophisticated newborn baby'
+  'Oh my God, I wonder who else was born in Eagleton. Voldemort, probably.'
 ];
 
 module.exports = quotes;
