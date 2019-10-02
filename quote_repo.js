3c4254c2c9f44cc8cda317de949e678f65e4d1d7
bisonwebmedia <46052692+bisonwebmedia@users.noreply.github.com>
2019-10-01 22:23:39 -0500

    Update quote_repo.js

--- quote_repo.js
+++ quote_repo.js
@@ -47,6 +47,7 @@ const quotes = [
   "No matter what I do, literally nothing bad can happen to me. I'm like a white male US senator.",
   'I would like to be president some day, so, no, I have not smoked marijuana.',
   'Every great work of art contains a message. And the message of this painting is get out of my way unless you want an arrow in your ass, Marcia.'
+  'I love you and I like you.'
 ];
 
 module.exports = quotes;
