a9ea6660e652e4885e77b7196f4b2e13c58cfa9b
Maggy Prajapati <mupraj10@gmail.com>
2019-10-01 22:01:57 -0400

    formatting

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-path-concat */
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
@@ -5,37 +6,34 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 1337;
 
-
 const sendQuote = require('./sendQuote');
 const quotes = require('./quote_repo');
-
-
 
 app.use(cors());
 
 app.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname + '/dance.gif'))
+  res.sendFile(path.join(__dirname + '/dance.gif'));
 });
 
 app.get('/quotes/:num?', (req, res, next) => {
-    const quoteRequested = req.params.num;
+  const quoteRequested = req.params.num;
 
-    quoteRequested > quotes.length 
+  quoteRequested > quotes.length
     ? res.sendFile(path.join(__dirname, '.', '/index.html'))
     : res.send(sendQuote(quoteRequested || 1));
 
-    next();
+  next();
 });
 
-app.use('*', (req, res) => {		 
-    res.sendFile(path.join(__dirname, '.', '/index.html'))
-  });
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '.', '/index.html'));
+});
 
 app.use((err, req, res, next) => {
-    console.error(err, err.stack);
-    res.status(500).send(err);
+  console.error(err, err.stack);
+  res.status(500).send(err);
 });
 
 app.listen(port, () => {
-	console.log('Mixing it up on port', port);
+  console.log('Mixing it up on port', port);
 });
--- sendQuote.js
+++ sendQuote.js
@@ -1,22 +1,15 @@
+const quotes = require("./quote_repo");
 
-const quotes = require('./quote_repo');
+function sendQuote(num) {
+  const quotesSent = [];
+  let quote;
 
-function sendQuote(num){
-    const quotesSent = [];
-    let quote;
+  for (let i = 0; i < num; i++) {
+    quote = quotes[Math.floor(Math.random() * quotes.length)];
+    quotesSent.push(quote);
+  }
 
-
-    for (let i = 0; i < num; i++){
-
-        quote = quotes[Math.floor(Math.random() * quotes.length)];
-        quotesSent.push(quote);
-
-    }
-
-    return quotesSent;
-
+  return quotesSent;
 }
 
-
 module.exports = sendQuote;
-
